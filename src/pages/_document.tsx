ec3db0f4817403bcbfca1a8577e3fe98eacb4788
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-03-28 19:22:23 -0400

    fix: link fonts google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,8 +42,15 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
