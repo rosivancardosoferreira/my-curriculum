e0d31cb91058cab5993cd8a5414c1c8eda223790
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-03-27 23:33:12 -0400

    Image metada

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -10,12 +10,14 @@ import {
   Footer
 } from "presentation/Sections";
 import { NavHeader, WhatsApp } from "presentation/Components";
+import { Images } from "assets/images";
 
 export function Home() {
   return (
     <Fragment>
       <Head>
         <title>Rosivan Cardoso | Portfólio</title>
+        <meta property="og:image" content={Images.Profile.src} />
       </Head>
       <NavHeader />
       <WhatsApp />
